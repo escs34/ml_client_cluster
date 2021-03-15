9674fd05e3ad5a37093f4a97987cfef280dac3dd
sts <escs34@kookmin.ac.kr>
2021-03-15 09:44:16 +0900

    test.sh and run.sh changed to doing task with loop processing

--- run.sh
+++ run.sh
@@ -6,11 +6,12 @@ num=$1
 post='.jpeg'
 
 img_path="${pre}${num}${post}"
+
 ###
 
 begin=$(date +%s%3N)
 ##
-curl -v -F file=img_path 'http://0.0.0.0:5000/upload_jpg'
+curl -v -F file=${img_path} 'http://0.0.0.0:5000/upload_jpg'
 end=$(date +%s%3N)
 
 result="${begin} ${end}"
--- test.sh
+++ test.sh
@@ -4,7 +4,27 @@ begin=$(date +%s%3N)
 
 rm log.txt
 
-seq 1 5 | xargs -n1 -P30 ./run.sh $1
+SET=$(seq 1 1)
+
+
+for i in $SET
+do
+	num_of_p=$i
+	data_json="{\"num_of_p\":\"${num_of_p}\"}"
+	
+	echo $data_json
+	
+	curl --header "Content-Type: application/json" \
+		--request POST \
+		--data ${data_json} \
+		http://0.0.0.0:5000/log
+
+	echo ${num_of_p}
+	sleep 5
+	seq 1 500 | xargs -n1 -P${num_of_p} ./run.sh $1
+
+	sleep 5
+done
 
 end=$(date +%s%3N)
 
