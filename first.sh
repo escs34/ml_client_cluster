42cc330ff2ca8140682d3b3b17d6780a23c923dc
escs34 <escs34@kookmin.ac.kr>
2021-03-17 04:53:51 +0000

    first.sh fixed

--- first.sh
+++ first.sh
@@ -2,29 +2,8 @@
   
 begin=$(date +%s%3N)
 
-rm log.txt
+seq 1 100 | xargs -n1 -P${num_of_p} ./run.sh $1
 
-SET=$(seq 20 20)
-
-
-for i in $SET
-do
-	num_of_p=$i
-	data_json="{\"num_of_p\":\"${num_of_p}\"}"
-	
-	echo $data_json
-	
-	curl --header "Content-Type: application/json" \
-		--request POST \
-		--data ${data_json} \
-		http://0.0.0.0:5000/log
-
-	echo ${num_of_p}
-	sleep 5
-	seq 1 100 | xargs -n1 -P${num_of_p} ./run.sh $1
-
-	sleep 5
-done
 
 end=$(date +%s%3N)
 
