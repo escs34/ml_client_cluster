0d97a9c23dec7ccbef99f96a821c0e4db10be88d
root <root@ip-172-31-38-47.us-west-2.compute.internal>
2021-03-17 04:37:45 +0000

    first.sh for warm up server added

---
+++ first.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+  
+begin=$(date +%s%3N)
+
+rm log.txt
+
+SET=$(seq 20 20)
+
+
+for i in $SET
+do
+	num_of_p=$i
+	data_json="{\"num_of_p\":\"${num_of_p}\"}"
+	
+	echo $data_json
+	
+	curl --header "Content-Type: application/json" \
+		--request POST \
+		--data ${data_json} \
+		http://0.0.0.0:5000/log
+
+	echo ${num_of_p}
+	sleep 5
+	seq 1 100 | xargs -n1 -P${num_of_p} ./run.sh $1
+
+	sleep 5
+done
+
+end=$(date +%s%3N)
+
+result=${end}-${begin}
+echo ${result}
